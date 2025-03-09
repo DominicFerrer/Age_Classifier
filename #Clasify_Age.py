857e45dad3f7e704c1a105c95d7e13def4f3dd73
Ferrer, John Dominic P. <johndominic.9valor@gmail.com>
2025-03-09 06:57:41 +0300

    Update #Clasify_Age.py

--- #Clasify_Age.py
+++ #Clasify_Age.py
@@ -1,22 +1,24 @@
 #Clasify_Age
 def age():
     age = int(input("Enter your age: "))
-    if age <= 12:
+    if age < 0: 
+        print("Invalid Age! Please Enter Value between 0 and 100")
+    elif age <=12:
         print("You are a CHILD.")
-    elif age <= 19:
-        print("Your are a TEEN.")
-    elif age <= 64:
+    elif age <=19:
+        print("You are a TEEN.")
+    elif age <=64:
         print("You are a ADULT.")
-    elif age >= 65:
+    elif age >=65:
         print("You are a SENIOR.")
 
 age()
 
 while True:
-    a = input("Run again?:")
+    a = input("Run Again?: ")
     if a == "Yes":
         age()
 
-    else:
-        b = input("CONFIRMED")
+    else: 
+        b = print("PROGRAM DONE")
         break
